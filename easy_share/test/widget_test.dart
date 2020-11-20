aa0e28f1e59551846fc6764399513e538b35b7d9
joanaferreira0011 <joanaferreira0011@gmail.com>
2020-11-20 08:45:04 +0000

    added commented text

--- easy_share/test/widget_test.dart
+++ easy_share/test/widget_test.dart
@@ -9,22 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:easy_share/main.dart';
+import 'package:easy_share/Screens/add_event.page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     await tester.pumpWidget(AddEventPage());
+//     await tester.tap(find.byType(RaisedButton));
+//     expect(dind.text().buttonText, Text("Your event was added!"));
+//   });
 }
